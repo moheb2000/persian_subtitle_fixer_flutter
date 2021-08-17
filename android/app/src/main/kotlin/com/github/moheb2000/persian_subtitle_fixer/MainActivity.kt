a8ef413d6ca5d05caa557fbba3cb1afe3f707bd5
Mohammad Ebrahimi <moh.ebrahimi2000@gmail.com>
2021-08-17 13:56:41 +0430

    Create base app with persian locale support

---
+++ android/app/src/main/kotlin/com/github/moheb2000/persian_subtitle_fixer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.moheb2000.persian_subtitle_fixer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:persian_fonts/persian_fonts.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Theme config
+      theme: ThemeData(
+        fontFamily: PersianFonts.Vazir.fontFamily,
+      ),
+
+      // Localization config
+      localizationsDelegates: [
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale("fa", "IR"),
+      ],
+      locale: Locale("fa", "IR"),
+    );
+  }
+}
