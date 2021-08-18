3e8d21435fa059fb8d17df23cba5902620490392
Mohammad Ebrahimi <moh.ebrahimi2000@gmail.com>
2021-08-18 14:44:10 +0430

    Fix ch, g, p and some arabic characters

--- lib/src/logic/logic.dart
+++ lib/src/logic/logic.dart
@@ -1,15 +1,16 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
-import 'package:enough_convert/enough_convert.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import './windows1256_persian.dart';
 
 class Logic {
   void fixSubtitleFile(List<PlatformFile> files) async {
     SharedPreferences pres = await SharedPreferences.getInstance();
     for (var file in files) {
       final List<int> firstFile = await File(file.path!).readAsBytes();
-      final String fixedString = Windows1256Codec().decode(firstFile);
+      final String fixedString = Windows1256PersianCodec().decode(firstFile);
       final lestFile = await File('${pres.getString('subtitlePath')}/[Fixed]${file.name}').create(recursive: true);
       lestFile.writeAsString(fixedString);
     }
